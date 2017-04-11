65d48439f4ab64da2b199de2bd866f522094e204
Jelmer Le Mair <jelmerlm1@gmail.com>
2017-04-11 10:02:35 +0200

    colorRandomizer begin
    later mee verder

---
+++ hombregato/Assets/scripts/ColorRandomizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour{
+    private Renderer render;
+    [SerializeField]
+    private Color[] color;
+
+    public void PutColor(int chosenColor)
+    {
+        render = GetComponent<Renderer>();
+        render.material.SetColor("_Color", color[chosenColor - 1]);
+    }
+    private void Update()
+    {
+        Debug.Log(chosenColor);
+        PutColor(Random.Range(1, 6));
+    }
+}
